64d2bf90963fb0e868d773455827e7b7b6b9b6a7
TheFubon <43171309+cssdrive@users.noreply.github.com>
2018-10-07 09:49:49 +0300

    Update owl.carousel.js

--- owl-carousel/js/owl.carousel.js
+++ owl-carousel/js/owl.carousel.js
@@ -1,13 +1,3 @@
-/*
- *  jQuery OwlCarousel v1.3.3
- *
- *  Copyright (c) 2013 Bartosz Wojciechowski
- *  http://www.owlgraphic.com/owlcarousel/
- *
- *  Licensed under MIT
- *
- */
-
 /*JS Lint helpers: */
 /*global dragMove: false, dragEnd: false, $, jQuery, alert, window, document */
 /*jslint nomen: true, continue:true */
